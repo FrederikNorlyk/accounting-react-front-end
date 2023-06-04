43ecdab9557b2b8ac4bdb2afcd91610bb43c789d
Frederik Norlyk <frederiknorlyk@gmail.com>
2023-06-04 16:06:40 +0200

    Added a proper login page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,35 +1,111 @@
 "use client";
 
-import { signIn } from "next-auth/react";
-import { useRef } from "react";
+import { SignInResponse, signIn } from "next-auth/react";
+import { useRef, useState } from "react";
 
 interface IProps {
   searchParams?: { [key: string]: string | string[] | undefined };
 }
 
-const LoginPage = ({ searchParams }: IProps) => {
-  const userName = useRef("");
-  const pass = useRef("");
+export default function LoginPage({ searchParams }: IProps) {
+  const username = useRef("");
+  const password = useRef("");
+  const [errorMessage, setErrorMessage] = useState<string | null>(null)
 
-  const onSubmit = async () => {
-    await signIn("credentials", {
-      username: userName.current,
-      password: pass.current,
-      redirect: true,
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    setErrorMessage(null)
+
+    const response = await signIn("credentials", {
+      username: username.current,
+      password: password.current,
+      redirect: false,
       callbackUrl: "/",
-    });
+    }) as SignInResponse;
+
+    if (!response?.ok || response.status != 200) {
+      if (response.error == 'CredentialsSignin') {
+        setErrorMessage('Invalid user or password');
+      } else if (response.error == 'fetch failed') {
+        setErrorMessage('Could not connect to API');
+      } else {
+        setErrorMessage(response.error as string);
+      }
+    }
   };
-  
+
   return (
-    <div className={"flex flex-col justify-center items-center  h-screen bg-gradient-to-br gap-1 from-cyan-300 to-sky-600"}>
-      {searchParams?.message && <p className="text-red-700 bg-red-100 py-2 px-5 rounded-md">{searchParams?.message}</p>}
-      <div className="px-7 py-4 shadow bg-white rounded-md flex flex-col gap-2">
-        <input type="text" onChange={(e: any) => (userName.current = e.target.value)} />
-        <input type="password" onChange={(e: any) => (pass.current = e.target.value)} />
-        <button onClick={onSubmit}>Login</button>
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            className="mx-auto h-10 w-auto"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt="Your Company"
+          />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign in to your account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" onSubmit={onSubmit}>
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                User
+              </label>
+              <div className="mt-2">
+                <input
+                  id="username"
+                  name="username"
+                  type="text"
+                  required
+                  onChange={(e: any) => (username.current = e.target.value)}
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                  Password
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  onChange={(e: any) => (password.current = e.target.value)}
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            {errorMessage && <p className="text-red-700 bg-red-100 py-2 px-5 rounded-md">{errorMessage}</p>}
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Not a member?{' '}
+            <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+              Create a user
+            </a>
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
-
-export default LoginPage;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -27,6 +27,10 @@ export default function NavBar() {
 	const pathname = usePathname() || '';
 	const { data: session } = useSession()
 
+	if (!session) return (
+		<></>
+	)
+
 	return (
 		<Disclosure as="nav" className="bg-gray-800">
 			{({ open }) => (
@@ -43,7 +47,7 @@ export default function NavBar() {
 									/>
 								</div>
 
-
+								{/* Menu items */}
 								<div className="hidden md:block">
 									<div className="ml-10 flex items-baseline space-x-4">
 										{navigation.map((item) => {
@@ -72,39 +76,31 @@ export default function NavBar() {
 							<div className="hidden md:block">
 								<div className="ml-4 flex items-center md:ml-6">
 									{/* Profile dropdown */}
-
-									{session ? (
-										<>
-											<Menu as="div" className="relative ml-3">
-												<div>
-													<Menu.Button className="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-														<span className="sr-only">Open user menu</span>
-														<img
-															className="h-8 w-8 rounded-full"
-															src={user.imageUrl}
-															alt=""
-														/>
-													</Menu.Button>
-												</div>
-												<Transition
-													as={Fragment}
-													enter="transition ease-out duration-100"
-													enterFrom="transform opacity-0 scale-95"
-													enterTo="transform opacity-100 scale-100"
-													leave="transition ease-in duration-75"
-													leaveFrom="transform opacity-100 scale-100"
-													leaveTo="transform opacity-0 scale-95"
-												>
-													<Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-															<SignOutButton isMenuItem={true}/>
-													</Menu.Items>
-												</Transition>
-											</Menu>
-										</>
-									) : (
-										<h1>TEMP REMOVE</h1>
-									)}
-
+									<Menu as="div" className="relative ml-3">
+										<div>
+											<Menu.Button className="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+												<span className="sr-only">Open user menu</span>
+												<img
+													className="h-8 w-8 rounded-full"
+													src={user.imageUrl}
+													alt=""
+												/>
+											</Menu.Button>
+										</div>
+										<Transition
+											as={Fragment}
+											enter="transition ease-out duration-100"
+											enterFrom="transform opacity-0 scale-95"
+											enterTo="transform opacity-100 scale-100"
+											leave="transition ease-in duration-75"
+											leaveFrom="transform opacity-100 scale-100"
+											leaveTo="transform opacity-0 scale-95"
+										>
+											<Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+												<SignOutButton isMenuItem={true} />
+											</Menu.Items>
+										</Transition>
+									</Menu>
 								</div>
 							</div>
 							<div className="-mr-2 flex md:hidden">
