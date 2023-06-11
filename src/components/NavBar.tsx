121596d4a2a073e4afec883d17f39ba0eb4fff71
Frederik Norlyk <frederiknorlyk@gmail.com>
2023-06-11 15:09:56 +0200

    Change icon color

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -35,7 +35,7 @@ export default function NavBar() {
 								<div className="flex-shrink-0">
 									<img
 										className="h-8 w-8"
-										src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+										src="https://tailwindui.com/img/logos/mark.svg?color=blue&shade=500"
 										alt="Your Company"
 									/>
 								</div>
